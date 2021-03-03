c1b14525bc96f886c57a1643f01a15ac340a6632
grankko <grankko@gmail.com>
2021-03-03 23:47:35 +0100

    label responds to busy state

---
+++ src/TeslaCamMap.UwpClient/Converters/BusyLabelConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace TeslaCamMap.UwpClient.Converters
+{
+    public class BusyLabelConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is bool && parameter is string)
+            {
+                bool boolValue = (bool)value;
+                string[] parameters = ((string)parameter).Split(new char[] { '|' });
+
+                if (boolValue)
+                    return parameters[0];
+                else
+                    return parameters[1];
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
