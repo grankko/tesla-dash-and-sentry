1183fc5010a5b8040084836aa826293ba123fe6a
grankko <grankko@gmail.com>
2021-03-04 22:12:50 +0100

    adds video playback and broken clip navigation

--- src/TeslaCamMap.UwpClient/EventDetailsPage.xaml.cs
+++ src/TeslaCamMap.UwpClient/EventDetailsPage.xaml.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using FFmpegInterop;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using TeslaCamMap.UwpClient.Model;
+using TeslaCamMap.UwpClient.ViewModels;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -22,14 +25,123 @@ namespace TeslaCamMap.UwpClient
     /// </summary>
     public sealed partial class EventDetailsPage : Page
     {
+        private FFmpegInteropMSS _leftFfmpegInterop;
+        private FFmpegInteropMSS _frontFfmpegInterop;
+        private FFmpegInteropMSS _rightFfmpegInterop;
+        private FFmpegInteropMSS _backFfmpegInterop;
+
+        private DispatcherTimer _timer;
+
         public EventDetailsPage()
         {
             this.InitializeComponent();
+            _timer = new DispatcherTimer();
+            _timer.Interval = TimeSpan.FromMilliseconds(500);
+            _timer.Tick += _timer_Tick;
         }
 
-        private void HyperlinkButton_Click(object sender, RoutedEventArgs e)
+        private void _timer_Tick(object sender, object e)
+        {
+            VideoSlider.Value = LeftPlayer.MediaPlayer.PlaybackSession.Position.TotalSeconds;
+        }
+
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            this.DataContext = new EventDetailsViewModel((UwpTeslaEvent)e.Parameter);
+
+            var model = (UwpTeslaEvent)e.Parameter;
+
+            var leftFile = model.Clips.Cast<UwpClip>().Where(c => c.Camera == Lib.Model.Camera.LeftRepeater).OrderBy(c => c.FileName).First();
+            var rightFile = model.Clips.Cast<UwpClip>().Where(c => c.Camera == Lib.Model.Camera.RightRepeater).OrderBy(c => c.FileName).First();
+            var frontFile = model.Clips.Cast<UwpClip>().Where(c => c.Camera == Lib.Model.Camera.Front).OrderBy(c => c.FileName).First();
+            var backFile = model.Clips.Cast<UwpClip>().Where(c => c.Camera == Lib.Model.Camera.Back).OrderBy(c => c.FileName).First();
+            
+            FFmpegInteropConfig conf = new FFmpegInteropConfig();
+            conf.DefaultBufferTime = TimeSpan.FromSeconds(60);
+
+            using (var stream = await leftFile.ClipFile.OpenAsync(Windows.Storage.FileAccessMode.Read))
+            {
+                _leftFfmpegInterop = await FFmpegInteropMSS.CreateFromStreamAsync(stream, conf);
+                LeftPlayer.Source = _leftFfmpegInterop.CreateMediaPlaybackItem();
+
+                VideoSlider.Minimum = 0;
+                VideoSlider.Maximum = _leftFfmpegInterop.Duration.TotalSeconds;
+            }
+
+            using (var stream = await rightFile.ClipFile.OpenAsync(Windows.Storage.FileAccessMode.Read))
+            {
+                _rightFfmpegInterop = await FFmpegInteropMSS.CreateFromStreamAsync(stream, conf);
+                RightPlayer.Source = _rightFfmpegInterop.CreateMediaPlaybackItem();
+            }
+
+            using (var stream = await frontFile.ClipFile.OpenAsync(Windows.Storage.FileAccessMode.Read))
+            {
+                _frontFfmpegInterop = await FFmpegInteropMSS.CreateFromStreamAsync(stream, conf);
+                FrontPlayer.Source = _frontFfmpegInterop.CreateMediaPlaybackItem();
+            }
+
+            using (var stream = await backFile.ClipFile.OpenAsync(Windows.Storage.FileAccessMode.Read))
+            {
+                _backFfmpegInterop = await FFmpegInteropMSS.CreateFromStreamAsync(stream, conf);
+                BackPlayer.Source = _backFfmpegInterop.CreateMediaPlaybackItem();
+            }
+        }
+
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            LeftPlayer.MediaPlayer.Play();
+            FrontPlayer.MediaPlayer.Play();
+            RightPlayer.MediaPlayer.Play();
+            BackPlayer.MediaPlayer.Play();
+
+            _timer.Start();
+        }
+
+        private void PauseButton_Click(object sender, RoutedEventArgs e)
+        {
+            LeftPlayer.MediaPlayer.Pause();
+            FrontPlayer.MediaPlayer.Pause();
+            RightPlayer.MediaPlayer.Pause();
+            BackPlayer.MediaPlayer.Pause();
+
+            _timer.Stop();
+        }
+
+        private void BackToMapButton_Click(object sender, RoutedEventArgs e)
         {
             this.Frame.Navigate(typeof(MainPage));
+        }
+
+        private void VideoSlider_ManipulationStarted(object sender, ManipulationStartedRoutedEventArgs e)
+        {
+            LeftPlayer.MediaPlayer.Pause();
+            FrontPlayer.MediaPlayer.Pause();
+            RightPlayer.MediaPlayer.Pause();
+            BackPlayer.MediaPlayer.Pause();
+
+            _timer.Stop();
+        }
+
+        private void VideoSlider_ManipulationCompleted(object sender, ManipulationCompletedRoutedEventArgs e)
+        {
+            var newValue = VideoSlider.Value;
+            LeftPlayer.MediaPlayer.PlaybackSession.Position = TimeSpan.FromSeconds(newValue);
+            FrontPlayer.MediaPlayer.PlaybackSession.Position = TimeSpan.FromSeconds(newValue);
+            RightPlayer.MediaPlayer.PlaybackSession.Position = TimeSpan.FromSeconds(newValue);
+            BackPlayer.MediaPlayer.PlaybackSession.Position = TimeSpan.FromSeconds(newValue);
+
+            LeftPlayer.MediaPlayer.Play();
+            FrontPlayer.MediaPlayer.Play();
+            RightPlayer.MediaPlayer.Play();
+            BackPlayer.MediaPlayer.Play();
+
+            _timer.Start();
+        }
+
+        private void ClipsListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ClipsListView.ScrollIntoView(ClipsListView.SelectedItem);
         }
     }
 }
---
+++ src/TeslaCamMap.UwpClient/ViewModels/EventDetailsViewModel.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeslaCamMap.Lib.Model;
+using TeslaCamMap.UwpClient.Commands;
+using TeslaCamMap.UwpClient.Model;
+
+namespace TeslaCamMap.UwpClient.ViewModels
+{
+    public class EventDetailsViewModel : ViewModelBase
+    {
+        private UwpTeslaEvent _model;
+
+        public RelayCommand NextClipCommand { get; set; }
+        public RelayCommand PreviousClipCommand { get; set; }
+        
+        public ObservableCollection<ClipViewModel> Clips { get; set; }
+
+        private ClipViewModel _currentClip;
+        public ClipViewModel CurrentClip
+        {
+            get { return _currentClip; }
+            set
+            {
+                _currentClip = value;
+                OnPropertyChanged();
+                NextClipCommand.RaiseCanExecuteChanged();
+                PreviousClipCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        public EventDetailsViewModel(UwpTeslaEvent model)
+        {
+            _model = model;
+
+            NextClipCommand = new RelayCommand(NextClipCommandExecute, CanNextClipCommandExecute);
+            PreviousClipCommand = new RelayCommand(PreviousClipCommandExecute, CanPreviousClipCommandExecute);
+
+            Clips = new ObservableCollection<ClipViewModel>();
+
+            var leftRepeaterClips = model.Clips.Where(c => c.Camera == Camera.LeftRepeater).OrderBy(c => c.FileName).ToList();
+            int index = 0;
+            foreach (var clip in leftRepeaterClips)
+            {
+                var clipViewModel = new ClipViewModel();
+                clipViewModel.FileName = clip.FileName.Substring(0, 19);
+                clipViewModel.ClipIndex = index;
+                Clips.Add(clipViewModel);
+                index++;
+            }
+
+            CurrentClip = Clips.First();
+        }
+
+        private bool CanPreviousClipCommandExecute(object arg)
+        {
+            return (CurrentClip != null && Clips.Min(c => c.ClipIndex) != CurrentClip.ClipIndex);
+        }
+
+        private void PreviousClipCommandExecute(object obj)
+        {
+            CurrentClip = Clips.Single(c => c.ClipIndex == (CurrentClip.ClipIndex - 1));
+        }
+
+        private bool CanNextClipCommandExecute(object arg)
+        {
+            return (CurrentClip != null && Clips.Max(c => c.ClipIndex) != CurrentClip.ClipIndex);
+        }
+
+        private void NextClipCommandExecute(object obj)
+        {
+            CurrentClip = Clips.Single(c => c.ClipIndex == (CurrentClip.ClipIndex + 1));
+        }
+    }
+
+    public class ClipViewModel : ViewModelBase
+    {
+        public string FileName { get; set; }
+        public int ClipIndex { get; set; }
+    }
+}
